cbc47bdb60daa9da4763bb7c734b0eceddf42ea8
SolbiShin <sbshin.dev@gmail.com>
2021-11-28 23:17:18 +0900

    immer를 사용하여 불변성 유지하기

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from "redux-actions";
+import produce from 'immer';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋값 변경
 const INSERT = 'todos/INSERT'; // 새로운 todo 등록
@@ -37,21 +38,24 @@ const initialState = {
 
 const todos = handleActions(
     {
-      [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input: input}),
-      [INSERT]: (state, { payload: todo }) => ({ 
-          ...state, 
-          todos: state.todos.concat(todo)
+      [CHANGE_INPUT]: (state, { payload: input }) => 
+        produce(state, draft => {
+            draft.input = input;
         }),
-        [TOGGLE]: (state, { payload: id }) => ({
-            ...state,
-            todos: state.todos.map(todo =>
-                todo.id ===  id? { ...todo, done: !todo.done } : todo )
+      [INSERT]: (state, { payload: todo }) =>
+        produce(state, draft => {
+            draft.todos.push(todo);
         }),
-        [REMOVE]: (state, { payload : id }) => ({
-            ...state,
-            todos: state.todos.filter(todo => 
-                todo.id !== id)
-        })
+        [TOGGLE]: (state, { payload: id }) => 
+          produce(state, draft => {
+            const todo = draft.todos.find(todo => todo.ind === id);
+            todo.done = !todo.done;
+          }),
+        [REMOVE]: (state, { payload : id }) =>
+          produce(state, draft => {
+              const index = draft.todos.findIndex(todo => todo.id === id);
+              draft.todos.splice(index, 1);
+          })
     },
     initialState
 );
